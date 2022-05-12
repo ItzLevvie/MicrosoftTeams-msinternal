48f561fa26b03a82eaba356b93a17357ac752c4e
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2022-05-12 20:38:53 +0100

    Update to the latest source code

--- src/v1.0/Exploration/msi/win-arm64.js
+++ src/v1.0/Exploration/msi/win-arm64.js
@@ -11,7 +11,7 @@ const getPlatform = "win-arm64";
 const getPath = "production-windows-arm64";
 const getFile = "Teams_windows_arm64.msi";
 
-for (let getLatestBuild = 13205, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
+for (let getLatestBuild = 13211, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
     https.get(getURL + "/" + getPath + "/" + getPartialBuild + getLatestBuild + "/" + getFile, (getResponse) => {
         if (getResponse.statusCode === 200) {
             const getDate = new Date(getResponse.headers["last-modified"]).toLocaleString("en-US", { dateStyle: "full", timeStyle: "short", timeZone: "PST" });
--- src/v1.0/Exploration/pkg/osx-x64.js
+++ src/v1.0/Exploration/pkg/osx-x64.js
@@ -11,7 +11,7 @@ const getPlatform = "osx-x64 + osx-arm64";
 const getPath = "production-osx";
 const getFile = "Teams_osx.pkg";
 
-for (let getLatestBuild = 13201, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
+for (let getLatestBuild = 13205, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
     https.get(getURL + "/" + getPath + "/" + getPartialBuild + getLatestBuild + "/" + getFile, (getResponse) => {
         if (getResponse.statusCode === 200) {
             const getDate = new Date(getResponse.headers["last-modified"]).toLocaleString("en-US", { dateStyle: "full", timeStyle: "short", timeZone: "PST" });
