d85cecddd58c6a7593d37eec6a1d73b8d9d27966
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2022-05-18 23:35:45 +0100

    Synchronize source code files

--- src/v1.0/Exploration/exe/win-x64.ps1
+++ src/v1.0/Exploration/exe/win-x64.ps1
@@ -18,9 +18,11 @@ for (($fda625b81e647d7c6acd91aed9ae8ecd = [int](Get-Content -Path "..\..\.github
             $ed87954bd831ca3d16ae52758005c27f = [System.Math]::Round($6f23ddfe07c161f84ec0b19efcc96f84)
             $bc9ca387c6d3ecefe4cbe9c390372057 = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date -Date $c28d3d1349dbbde7c419ba13df245e25), "Pacific Standard Time")
             $cd34ed983a00789e278cf0186c460fcf = $bc9ca387c6d3ecefe4cbe9c390372057.toString("dddd") + ", " + $bc9ca387c6d3ecefe4cbe9c390372057.toString("MMMM d") + ", " + $bc9ca387c6d3ecefe4cbe9c390372057.toString("yyyy") + " at " + $bc9ca387c6d3ecefe4cbe9c390372057.toString("h:mm tt")
-            $ab3b0e88a3e2899c2990b7cab3d3c26f = $null
         }
+        $ab3b0e88a3e2899c2990b7cab3d3c26f = $null
     }
 }
 
 Write-Output "1.5.00.$c05e6f4daf8c300393e85febda03e9fc ($fe6fde81dcb0a79dd8310604ca4b266a) - published on $cd34ed983a00789e278cf0186c460fcf with $ed87954bd831ca3d16ae52758005c27f MB: $a119da4b5a9f3e8851e58d4d9a37604a/$b3a439fdbf1a6ecc01de6445ee82b547/1.5.00.$c05e6f4daf8c300393e85febda03e9fc/$ae633e497ada527ef7c57413d1d1ac3c"
+
+Remove-Variable * -ErrorAction SilentlyContinue
