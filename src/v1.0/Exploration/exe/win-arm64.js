0a6950897b2c771ed0212e00962b7676d3535425
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2022-05-24 13:12:59 +0100

    Update to the latest source code

--- src/v1.0/Exploration/exe/win-arm64.js
+++ src/v1.0/Exploration/exe/win-arm64.js
@@ -11,7 +11,7 @@ const getPlatform = "win-arm64";
 const getPath = "production-windows-arm64";
 const getFile = "Teams_windows_arm64.exe";
 
-for (let getLatestBuild = 14322, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
+for (let getLatestBuild = 14404, getBuildLimit = getLatestBuild + 15; getLatestBuild <= getBuildLimit; getLatestBuild++) {
     https.get(getURL + "/" + getPath + "/" + getPartialBuild + getLatestBuild + "/" + getFile, (getResponse) => {
         if (getResponse.statusCode === 200) {
             const getDate = new Date(getResponse.headers["last-modified"]).toLocaleString("en-US", { dateStyle: "full", timeStyle: "short", timeZone: "PST" });
