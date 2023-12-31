2fbfc83d429c3cd9e0cbeeab120d165517736f9c
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2023-12-31 14:09:22 +0000

    Some changes (48)

--- src/v1.0/Daily/msi/win-arm64.ps1
+++ src/v1.0/Daily/msi/win-arm64.ps1
@@ -6,7 +6,7 @@ $b3a439fdbf1a6ecc01de6445ee82b547 = "production-windows-arm64"
 $ae633e497ada527ef7c57413d1d1ac3c = "Teams_windows_arm64.msi"
 $fe6fde81dcb0a79dd8310604ca4b266a = "win-arm64"
 
-for (($fda625b81e647d7c6acd91aed9ae8ecd = [int](Get-Content -Path "..\..\.github\ci\v1.0\Continuous Deployment\msi\$fe6fde81dcb0a79dd8310604ca4b266a.txt")), ($f62d19962ff48160c9e1e0332e7ef11c = $fda625b81e647d7c6acd91aed9ae8ecd + 148); $fda625b81e647d7c6acd91aed9ae8ecd -le $f62d19962ff48160c9e1e0332e7ef11c; $fda625b81e647d7c6acd91aed9ae8ecd++) {
+for (($fda625b81e647d7c6acd91aed9ae8ecd = [int](Get-Content -Path "..\..\.github\ci\v1.0\Daily\msi\$fe6fde81dcb0a79dd8310604ca4b266a.txt")), ($f62d19962ff48160c9e1e0332e7ef11c = $fda625b81e647d7c6acd91aed9ae8ecd + 148); $fda625b81e647d7c6acd91aed9ae8ecd -le $f62d19962ff48160c9e1e0332e7ef11c; $fda625b81e647d7c6acd91aed9ae8ecd++) {
     if ($fda625b81e647d7c6acd91aed9ae8ecd -match "^(\d+[5-9][0-9])$") {
         if ($fda625b81e647d7c6acd91aed9ae8ecd -match "^(\d+50)$") {
             continue
@@ -15,7 +15,7 @@ for (($fda625b81e647d7c6acd91aed9ae8ecd = [int](Get-Content -Path "..\..\.github
         if ($ab3b0e88a3e2899c2990b7cab3d3c26f) {
             $c05e6f4daf8c300393e85febda03e9fc = @()
             $c05e6f4daf8c300393e85febda03e9fc += $fda625b81e647d7c6acd91aed9ae8ecd
-            Write-Output "$c05e6f4daf8c300393e85febda03e9fc" > "..\..\.github\ci\v1.0\Continuous Deployment\msi\$fe6fde81dcb0a79dd8310604ca4b266a.txt"
+            Write-Output "$c05e6f4daf8c300393e85febda03e9fc" > "..\..\.github\ci\v1.0\Daily\msi\$fe6fde81dcb0a79dd8310604ca4b266a.txt"
             $c28d3d1349dbbde7c419ba13df245e25 = [string]$ab3b0e88a3e2899c2990b7cab3d3c26f.Headers["Last-Modified"]
             $6f23ddfe07c161f84ec0b19efcc96f84 = [string]$ab3b0e88a3e2899c2990b7cab3d3c26f.Headers["Content-Length"] / 1024 / 1024
             $ed87954bd831ca3d16ae52758005c27f = [System.Math]::Round($6f23ddfe07c161f84ec0b19efcc96f84)
