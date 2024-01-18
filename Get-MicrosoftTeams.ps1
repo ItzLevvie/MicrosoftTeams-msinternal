668a465a4ecc3ccdbec0e1c356493d21f100b3ee
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-01-18 23:17:24 +0000

    Some code fixes

--- Get-MicrosoftTeams.ps1
+++ Get-MicrosoftTeams.ps1
@@ -94,13 +94,11 @@ if ($Platform -eq "win-x64") {
     }
 }
 elseif ($Platform -eq "win-x86") {
-    if ($Version -eq "1.0") {
-        if ($Type -eq "Desktop") {
+    if ($Type -eq "Desktop" -or $Type -eq "Rooms") {
+        if ($Version -eq "1.0") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows"
         }
-    }
-    elseif ($Version -eq "2.0" -or $Version -eq "2.1") {
-        if ($Type -eq "Desktop" -or $Type -eq "Rooms") {
+        elseif ($Version -eq "2.0" -or $Version -eq "2.1") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x86"
         }
     }
