e7cca0383abc69a7bc6fc3ce6d35e43f68db9b6b
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-01-19 00:12:09 +0000

    Update Get-MicrosoftTeams.ps1

--- Get-MicrosoftTeams.ps1
+++ Get-MicrosoftTeams.ps1
@@ -188,7 +188,7 @@ while ($e49bb609ef26353d2c308ca6beb0de8f -le 32) {
             elseif ($Type -eq "Desktop" -and $Platform -eq "osx-x64 + osx-arm64") {
                 $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.macOS.latestVersion
             }
-            elseif ($Type -eq "Rooms" -and $Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
+            if ($Type -eq "Rooms" -and $Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
                 $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.MTRW.x64.latestVersion
             }
         }
