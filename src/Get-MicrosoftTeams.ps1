c4466efa86da0c7e267549ad2b4aaefda9b41e42
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-02-13 15:20:00 +0000

    Slight code-cleanup

--- src/Get-MicrosoftTeams.ps1
+++ src/Get-MicrosoftTeams.ps1
@@ -15,7 +15,7 @@ param (
     $Platform,
 
     [Parameter(Mandatory = $true)]
-    [ValidateSet("1.0", "2.0", "2.1")]
+    [ValidateSet("1.0", "2.0", "2.1", IgnoreCase = $false)]
     [string]
     $Version,
 
@@ -43,261 +43,202 @@ param (
 $ErrorActionPreference = "SilentlyContinue"
 $ProgressPreference = "SilentlyContinue"
 
-$Environment = $Environment.ToLower()
-$Ring = $Ring.ToLower()
-$Platform = $Platform.ToLower()
-$Version = $Version.ToLower()
-$Type = $Type.ToLower()
 $ObjectId = $ObjectId.ToLower()
 $TenantId = $TenantId.ToLower()
 
 #
-if ($Environment -eq "dev") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.teams.microsoft.com"
+if ($Environment -eq "dev" -or $Environment -eq "prod" -or $Environment -eq "life" -or $Environment -eq "gcc") {
+    $ecsUrl = "https://config.teams.microsoft.com"
 }
-elseif ($Environment -eq "prod") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.teams.microsoft.com"
+if ($Environment -eq "gcchigh") {
+    $ecsUrl = "https://config.ecs.gov.teams.microsoft.us"
 }
-elseif ($Environment -eq "life") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.teams.microsoft.com"
+if ($Environment -eq "dod") {
+    $ecsUrl = "https://config.ecs.dod.teams.microsoft.us"
 }
-elseif ($Environment -eq "gcc") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.teams.microsoft.com"
-}
-elseif ($Environment -eq "gcchigh") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.ecs.gov.teams.microsoft.us"
-}
-elseif ($Environment -eq "dod") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://config.ecs.dod.teams.microsoft.us"
-}
-elseif ($Environment -eq "gallatin") {
-    $dce146e66b2e5c0e104729239cc1ae15 = "https://mooncake.config.teams.microsoft.com"
+if ($Environment -eq "gallatin") {
+    $ecsUrl = "https://mooncake.config.teams.microsoft.com"
 }
 
 #
-if ($Ring -eq "ring0") {
-    $e783048973935431707b367c535d6ea4 = "https://staticsint.teams.cdn.office.net"
+if ($Ring -eq "ring0" -or $Ring -eq "ring0_s" -or $Ring -eq "ring1") {
+    $staticsPath = "https://staticsint.teams.cdn.office.net"
+    if ($Client -eq "Xbox") {
+        $staticsPath = "https://statics.teams.cdn.office.net"
+        if ($Ring -eq "ring0_s") {
+            $Ring = "ring0"
+        }
+    }
 }
-elseif ($Ring -eq "ring0_s") {
-    $e783048973935431707b367c535d6ea4 = "https://staticsint.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring1") {
-    $e783048973935431707b367c535d6ea4 = "https://staticsint.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring1_5") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring1_6") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring2") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring3") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring3_6") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "ring3_9") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "general") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-elseif ($Ring -eq "general_gcc") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
+if ($Ring -eq "ring1_5" -or $Ring -eq "ring1_6" -or $Ring -eq "ring2" -or $Ring -eq "ring3" -or $Ring -eq "ring3_6" -or $Ring -eq "ring3_9" -or $Ring -eq "general" -or $Ring -eq "general_gcc") {
+    $staticsPath = "https://statics.teams.cdn.office.net"
 }
 
 #
 if ($Version -eq "1.0") {
     if ($Client -eq "Desktop") {
+        $platformId = "27"
         if ($Platform -eq "win-x64") {
             if ($Type -eq "exe") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-x64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_x64.exe"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msi") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-x64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_x64.msi"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msix") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-msix-x64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_x64.msix"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
         }
         if ($Platform -eq "win-x86") {
             if ($Type -eq "exe") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows.exe"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msi") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows.msi"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msix") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-msix"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows.msix"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
         }
         if ($Platform -eq "win-arm64") {
             if ($Type -eq "exe") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-arm64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_arm64.exe"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msi") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-arm64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_arm64.msi"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
             if ($Type -eq "msix") {
                 $bd3078f40b0117196fdb4853563084e9 = "production-windows-msix-arm64"
                 $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_windows_arm64.msix"
-                $c53cc331b8934004bd5807be1b2b345e = "27"
             }
         }
         if ($Platform -eq "osx-x64 + osx-arm64") {
+            $platformId = "28"
             $bd3078f40b0117196fdb4853563084e9 = "production-osx"
             $e7782dde7d9e6e4f63894a63138afbb9 = "Teams_osx.pkg"
-            $c53cc331b8934004bd5807be1b2b345e = "28"
         }
     }
 }
-elseif ($Version -eq "2.0") {
+if ($Version -eq "2.0") {
+    $platformId = "48"
     if ($Client -eq "Desktop") {
         if ($Platform -eq "win-x64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MicrosoftTeams-x64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "48"
         }
         if ($Platform -eq "win-x86") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x86"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MicrosoftTeams-x86.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "48"
         }
         if ($Platform -eq "win-arm64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-arm64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MicrosoftTeams-arm64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "48"
         }
     }
     if ($Client -eq "Xbox") {
         if ($Platform -eq "win-x64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-xbox-x64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeamsPlayTogether_x64.msixbundle"
-            $c53cc331b8934004bd5807be1b2b345e = "48"
         }
         if ($Platform -eq "win-x86") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-xbox-x86"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeamsPlayTogether_x86.msixbundle"
-            $c53cc331b8934004bd5807be1b2b345e = "48"
         }
     }
 }
-elseif ($Version -eq "2.1") {
+if ($Version -eq "2.1") {
     if ($Client -eq "Desktop") {
+        $platformId = "49"
         if ($Platform -eq "win-x64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-x64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "49"
         }
         if ($Platform -eq "win-x86") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x86"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-x86.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "49"
         }
         if ($Platform -eq "win-arm64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-arm64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-arm64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "49"
         }
         if ($Platform -eq "osx-x64 + osx-arm64") {
+            $platformId = "50"
             $bd3078f40b0117196fdb4853563084e9 = "production-osx"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MicrosoftTeams.pkg"
-            $c53cc331b8934004bd5807be1b2b345e = "50"
         }
     }
     if ($Client -eq "Rooms") {
+        $platformId = "51"
         if ($Platform -eq "win-x64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-Rooms-x64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "51"
         }
         if ($Platform -eq "win-x86") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-x86"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-Rooms-x86.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "51"
         }
         if ($Platform -eq "win-arm64") {
             $bd3078f40b0117196fdb4853563084e9 = "production-windows-arm64"
             $e7782dde7d9e6e4f63894a63138afbb9 = "MSTeams-Rooms-arm64.msix"
-            $c53cc331b8934004bd5807be1b2b345e = "51"
         }
     }
 }
 
 #
-if ($Client -eq "Xbox") {
-    $e783048973935431707b367c535d6ea4 = "https://statics.teams.cdn.office.net"
-}
-if ($Client -eq "Xbox" -and $Ring -eq "ring0_s") {
-    $Ring = "ring0"
-}
-
-#
-$e49bb609ef26353d2c308ca6beb0de8f = 0
-while ($e49bb609ef26353d2c308ca6beb0de8f -le 32) {
+$i = 0
+while ($i -le 32) {
     try {
-        $dfd1dfb880a3f7093614df7cc6364a33 = (Invoke-RestMethod -Uri "$dce146e66b2e5c0e104729239cc1ae15/config/v1/MicrosoftTeams/$($c53cc331b8934004bd5807be1b2b345e)_1.0.0.0?environment=$Environment&audienceGroup=$Ring&teamsRing=$Ring&id=$ObjectId&tenantId=$TenantId&agent=TeamsBuilds" -Method Get).BuildSettings
+        $dfd1dfb880a3f7093614df7cc6364a33 = (Invoke-RestMethod -Uri "$ecsUrl/config/v1/MicrosoftTeams/$($platformId)_1.0.0.0?environment=$Environment&audienceGroup=$Ring&teamsRing=$Ring&id=$ObjectId&tenantId=$TenantId&agent=TeamsBuilds" -Method Get).BuildSettings
         if ($Version -eq "1.0") {
             if ($Client -eq "Desktop") {
                 if ($Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.Desktop.windows64.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.Desktop.windows64.latestVersion
                 }
                 if ($Platform -eq "osx-x64 + osx-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.Desktop.osx.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.Desktop.osx.latestVersion
                 }
             }
         }
-        elseif ($Version -eq "2.0") {
+        if ($Version -eq "2.0") {
             if ($Client -eq "Desktop") {
                 if ($Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2.x64.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.WebView2.x64.latestVersion
                 }
             }
             if ($Client -eq "Xbox") {
                 if ($Platform -eq "win-x64" -or $Platform -eq "win-x86") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.GamebarWidget.x64.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.GamebarWidget.x64.latestVersion
                 }
             }
         }
-        elseif ($Version -eq "2.1") {
+        if ($Version -eq "2.1") {
             if ($Client -eq "Desktop") {
                 if ($Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.x64.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.x64.latestVersion
                 }
                 if ($Platform -eq "osx-x64 + osx-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.macOS.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.macOS.latestVersion
                 }
             }
             if ($Client -eq "Rooms") {
                 if ($Platform -eq "win-x64" -or $Platform -eq "win-x86" -or $Platform -eq "win-arm64") {
-                    $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.MTRW.x64.latestVersion
-                    if ($null -eq $a553ddde23e7dad4144c98d2e342ba31) {
-                        $a553ddde23e7dad4144c98d2e342ba31 = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.x64.latestVersion
+                    $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.MTRW.x64.latestVersion
+                    if ($null -eq $buildVersion) {
+                        $buildVersion = $dfd1dfb880a3f7093614df7cc6364a33.WebView2Canary.x64.latestVersion
                     }
                 }
             }
         }
-        if ($a553ddde23e7dad4144c98d2e342ba31) {
-            $b0d2ed7ea6bb774a6b2c75f7f3a121eb = (Invoke-WebRequest -Uri "$e783048973935431707b367c535d6ea4/$bd3078f40b0117196fdb4853563084e9/$a553ddde23e7dad4144c98d2e342ba31/$e7782dde7d9e6e4f63894a63138afbb9" -Method Head)
+        if ($buildVersion) {
+            $b0d2ed7ea6bb774a6b2c75f7f3a121eb = (Invoke-WebRequest -Uri "$staticsPath/$bd3078f40b0117196fdb4853563084e9/$buildVersion/$e7782dde7d9e6e4f63894a63138afbb9" -Method Head)
 
             $2d12ae434d57fdb66f008265c8629877 = [string]$b0d2ed7ea6bb774a6b2c75f7f3a121eb.Headers["Last-Modified"]
             $5285c2f58cbd36531882b608e7e478e2 = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date -Date $2d12ae434d57fdb66f008265c8629877), "Pacific Standard Time")
@@ -306,11 +247,11 @@ while ($e49bb609ef26353d2c308ca6beb0de8f -le 32) {
             $abad549e7d030b408b70f72f08de798a = [string]$b0d2ed7ea6bb774a6b2c75f7f3a121eb.Headers["Content-Length"] / 1024 / 1024
             $e0167d390ca0b503d10f31ed2ded6eb4 = [System.Math]::Round($abad549e7d030b408b70f72f08de798a)
 
-            Write-Output "  $a553ddde23e7dad4144c98d2e342ba31 ($Platform) - published on $e1e240488e5eef1f776376b0ebf7609a with $e0167d390ca0b503d10f31ed2ded6eb4 MB: $e783048973935431707b367c535d6ea4/$bd3078f40b0117196fdb4853563084e9/$a553ddde23e7dad4144c98d2e342ba31/$e7782dde7d9e6e4f63894a63138afbb9"
+            Write-Output "  $buildVersion ($Platform) - published on $e1e240488e5eef1f776376b0ebf7609a with $e0167d390ca0b503d10f31ed2ded6eb4 MB: $staticsPath/$bd3078f40b0117196fdb4853563084e9/$buildVersion/$e7782dde7d9e6e4f63894a63138afbb9"
         }
         break
     }
     catch {
-        $e49bb609ef26353d2c308ca6beb0de8f++
+        $i++
     }
 }
