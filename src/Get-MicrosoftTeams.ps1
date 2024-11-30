8c3ef09f9a785171a3b4efc976674b9d7e1f223f
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-11-30 14:13:31 +0000

    Script bug fixes from 30th of November 2024

--- src/Get-MicrosoftTeams.ps1
+++ src/Get-MicrosoftTeams.ps1
@@ -52,6 +52,12 @@ if ($Environment -eq "gallatin") {
     $ecsUrl = "https://mooncake.config.teams.microsoft.com"
 }
 
+# Script bug fixes from 30th of November 2024
+if ($Environment -eq "gallatin" -and $Ring -eq "general" -and $Platform -eq "osx-x64 + osx-arm64") {
+    $Environment = "prod"
+    $Ring = "general_gcc"
+}
+
 #
 if ($Ring -eq "ring0" -or $Ring -eq "ring0_s" -or $Ring -eq "ring1") {
     $staticsUrl = "https://staticsint.teams.cdn.office.net"
