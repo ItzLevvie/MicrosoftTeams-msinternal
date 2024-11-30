a80af34dbd2eec78b872b2741ba291caed8837fc
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-11-30 14:52:40 +0000

    Updates

--- src/Get-MicrosoftTeams.ps1
+++ src/Get-MicrosoftTeams.ps1
@@ -59,7 +59,7 @@ if ($Environment -eq "life" -and $Ring -eq "ring0" -and $Version -eq "2.0" -and 
 if ($Environment -eq "life" -and $Ring -eq "ring0_s" -and $Version -eq "2.0" -and $Client -eq "Desktop") {
     $Environment = "prod"
 }
-if ($Environment -eq "gallatin" -and $Ring -eq "general" -and $Platform -eq "osx-x64 + osx-arm64" -and $Version -eq "1.0" -and $Client -eq "Desktop" -and $Type -eq "exe") {
+if ($Environment -eq "gallatin" -and $Ring -eq "general" -and $Platform -eq "osx-x64 + osx-arm64" -and $Version -eq "1.0" -and $Client -eq "Desktop") {
     $Environment = "prod"
     $Ring = "general_gcc"
 }
